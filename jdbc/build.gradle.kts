0de7ef849582d82f355c4f80df136846112b3157
Jim Schneidereit <schneidereit@pm.me>
2025-05-07 15:33:46 -0700

    fix (dbeaver): make shaded jar default and increase driver registration logging

--- jdbc/build.gradle.kts
+++ jdbc/build.gradle.kts
@@ -20,39 +20,35 @@ dependencies {
 }
 
 tasks.shadowJar {
-    val shadeBase = "com.salesforce.datacloud.jdbc.internal.shaded"
+    val shadeBase = "com.salesforce.datacloud.shaded"
 
     archiveBaseName = "jdbc"
-    archiveClassifier = "shaded"
-
+    archiveClassifier = ""
     duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+
+    relocate("com.google", "$shadeBase.com.google")
+    relocate("io.grpc", "$shadeBase.io.grpc")
+    relocate("com.fasterxml.jackson", "$shadeBase.com.fasterxml.jackson")
+    relocate("dev.failsafe", "$shadeBase.dev.failsafe")
+    relocate("io.jsonwebtoken", "$shadeBase.io.jsonwebtoken")
+    relocate("io.netty", "$shadeBase.io.netty")
+    relocate("kotlin", "$shadeBase.kotlin")
+    relocate("okhttp3", "$shadeBase.okhttp3")
+    relocate("okio", "$shadeBase.okio")
+    relocate("org.apache.arrow", "$shadeBase.org.apache.arrow")
+    relocate("org.apache.calcite", "$shadeBase.org.apache.calcite") {
+        exclude("org.apache.calcite.avatica.remote.Driver")
+    }
+    relocate("org.apache.commons", "$shadeBase.org.apache.commons")
+    relocate("org.apache.hc", "$shadeBase.org.apache.hc")
 
     mergeServiceFiles {
         exclude("META-INF/services/java.sql.Driver")
     }
+    
+    exclude("org.slf4j")
 
-    relocate("org.apache.calcite.avatica", "$shadeBase.org.apache.calcite.avatica") {
-        exclude("org.apache.calcite.avatica.remote.Driver")
-    }
-
-    relocate("org.apache", "$shadeBase.org.apache")
-    relocate("salesforce.cdp.hyperdb", "$shadeBase.salesforce.cdp.hyperdb")
-    relocate("okio", "$shadeBase.okio")
-    relocate("okhttp3", "$shadeBase.okhttp3")
-    relocate("io.netty", "$shadeBase.io.netty")
-    relocate("io.grpc", "$shadeBase.io.grpc")
-    relocate("com.fasterxml.jackson", "$shadeBase.com.fasterxml.jackson")
-    relocate("io.jsonwebtoken", "$shadeBase.io.jsonwebtoken")
-    relocate("com.squareup", "$shadeBase.com.squareup")
-    relocate("com.google", "$shadeBase.com.google")
-    relocate("net.jodah", "$shadeBase.net.jodah")
-    relocate("org.projectlombok", "$shadeBase.org.projectlombok")
-    relocate("javax.annotation", "$shadeBase.javax.annotation")
-    relocate("com.google.protobuf", "$shadeBase.com.google.protobuf")
-    relocate("org.slf4j", "$shadeBase.org.slf4j")
-    relocate("lombok", "$shadeBase.lombok")
-    relocate("kotlin", "$shadeBase.kotlin")
-
+    exclude("org.apache.calcite.avatica.remote.Driver")
     exclude("META-INF/LICENSE*")
     exclude("META-INF/NOTICE*")
     exclude("META-INF/DEPENDENCIES")
@@ -70,8 +66,27 @@ tasks.shadowJar {
     exclude("dependencies.properties")
     exclude("**/*.proto")
     exclude("arrow-git.properties")
+
+    shouldRunAfter(tasks.jar)
+}
+
+val copyShadedJar = tasks.register<Copy>("copyShadedJar") {
+    dependsOn(tasks.shadowJar)
+    from(tasks.shadowJar.get().archiveFile)
+    into(layout.buildDirectory.dir("libs"))
+    rename { it.replace(".jar", "-shaded.jar") }
+}
+
+tasks.jar {
+    archiveClassifier = "original"
 }
 
 tasks.named("compileJava") {
     dependsOn(":jdbc-core:build")
+}
+
+tasks.build {
+    dependsOn(tasks.jar)
+    dependsOn(tasks.shadowJar)
+    dependsOn(copyShadedJar)
 }
